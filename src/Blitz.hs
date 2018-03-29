1177b1b13ab47857f92a832ae9dcd577bdc98cff
Chris Done <chrisdone@gmail.com>
2018-03-29 18:29:51 +0100

    Cleanup

--- src/Blitz.hs
+++ src/Blitz.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BangPatterns #-}
 
--- |
+-- | A fast CSV parser.
 
 module Blitz where
 
@@ -21,67 +21,49 @@ parseByteString bytes0 = fmap ($ []) (dispatch bytes0 id id)
     dispatch bytes columns rows =
       case S8.uncons bytes of
         Nothing -> Right (rows . (columns [] :))
-        Just ('"', bytes') ->
-          let string extended dropped =
-                case S8.elemIndex '"' (S.drop dropped bytes') of
-                  Nothing -> error "Unterminated quoted field."
-                  Just idx ->
-                    case S8.uncons (S.drop (dropped + idx + 1) bytes') of
-                      Just ('"', _) -> string True (dropped + idx + 2)
-                      Just (',', _) ->
-                        let prefinalStr = (S.take (dropped + idx) bytes')
-                            finalStr =
-                              if extended
-                                then S.intercalate
-                                       "\""
-                                       (filter
-                                          (not . S.null)
-                                          (S8.split '"' prefinalStr))
-                                else prefinalStr
-                        in dispatch
-                             (S.drop (dropped + idx + 2) bytes')
-                             (columns . (finalStr :))
-                             rows
-                      Just ('\n', _) ->
-                        let prefinalStr = (S.take (dropped + idx) bytes')
-                            finalStr =
-                              if extended
-                                then S.intercalate
-                                       "\""
-                                       (filter
-                                          (not . S.null)
-                                          (S8.split '"' prefinalStr))
-                                else prefinalStr
-                        in dispatch
-                             (S.drop (dropped + idx + 2) bytes')
-                             id
-                             (rows . ((columns . (finalStr :)) [] :))
-                      Just {} -> Left "Expected end of file."
-                      Nothing -> Left "End of file?"
-          in string False 0
-        Just _ ->
-          case (S8.elemIndex ',' bytes, S8.elemIndex '\n' bytes) of
-            (Just comma, Just n)
-              | comma < n ->
-                let finalStr = S.take comma bytes
-                in dispatch
-                     (S.drop (comma + 1) bytes)
-                     (columns . (finalStr :))
-                     rows
-              | otherwise ->
-                let finalStr = S.take n bytes
-                in dispatch
-                     (S.drop (n + 1) bytes)
-                     id
-                     (rows . ((columns . (finalStr :)) [] :))
-            (Nothing, Just n) ->
-              Right (rows . ((columns . (S.take n bytes :)) [] :))
-            (Nothing, Nothing) ->
-              Right
-                (rows . ((columns . (S.take (S.length bytes) bytes :)) [] :))
-            (Just comma, Nothing) ->
-              let finalStr = S.take comma bytes
-              in dispatch
-                   (S.drop (comma + 1) bytes)
-                   (columns . (finalStr :))
-                   rows
+        Just ('"', bytes') -> quoted columns rows bytes' False 0
+        Just _ -> unquoted bytes columns rows
+    unquoted bytes columns rows =
+      case (S8.elemIndex ',' bytes, S8.elemIndex '\n' bytes) of
+        (Just comma, Just n)
+          | comma < n ->
+            let finalStr = S.take comma bytes
+            in dispatch (S.drop (comma + 1) bytes) (columns . (finalStr :)) rows
+          | otherwise ->
+            let finalStr = S.take n bytes
+            in dispatch
+                 (S.drop (n + 1) bytes)
+                 id
+                 (rows . ((columns . (finalStr :)) [] :))
+        (Nothing, Just n) ->
+          Right (rows . ((columns . (S.take n bytes :)) [] :))
+        (Nothing, Nothing) ->
+          Right (rows . ((columns . (S.take (S.length bytes) bytes :)) [] :))
+        (Just comma, Nothing) ->
+          let finalStr = S.take comma bytes
+          in dispatch (S.drop (comma + 1) bytes) (columns . (finalStr :)) rows
+    quoted columns rows bytes' extended dropped =
+      case S8.elemIndex '"' (S.drop dropped bytes') of
+        Nothing -> error "Unterminated quoted field."
+        Just idx ->
+          case S8.uncons (S.drop (nextQuoteIdx + 1) bytes') of
+            Just ('"', _) -> quoted columns rows bytes' True (nextQuoteIdx + 2)
+            Just (',', _) -> finishStringBeforeDelim
+            Just ('\n', _) -> finishStringBeforeDelim
+            Just {} -> Left "Expected end of file."
+            Nothing -> Left "End of file?"
+          where nextQuoteIdx = dropped + idx
+                finishStringBeforeDelim =
+                  let prefinalStr = S.take nextQuoteIdx bytes'
+                      finalStr =
+                        if extended
+                          then S.intercalate
+                                 "\""
+                                 (filter
+                                    (not . S.null)
+                                    (S8.split '"' prefinalStr))
+                          else prefinalStr
+                  in dispatch
+                       (S.drop (nextQuoteIdx + 2) bytes')
+                       (columns . (finalStr :))
+                       rows
