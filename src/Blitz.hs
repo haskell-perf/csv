9a4bf7f37eccda83bd76466c07e73e9596b435eb
Chris Done <chrisdone@gmail.com>
2018-03-29 17:46:40 +0100

    Switch to lists of lists
    Progress: 1/2
    Case                                         Allocated        Max        Live  GCs
    blitz/parseFile                              2,815,920     10,280      24,256    2
    cassava/decode/Vector ByteString             8,248,240      9,200      23,584    4
    cassava/decode/[ByteString]                 10,733,192      9,128      23,392    6
    lazy-csv/parseCsv/[ByteString]              17,848,264  1,121,216   1,191,072   16
    csv-conduit/readCSVFile/[ByteString]        26,996,264  1,204,088   1,249,992   25
    csv-conduit/readCSVFile/Vector ByteString   27,925,016  1,199,352   1,245,680   26
    csv-conduit/readCSVFile/[String]            34,269,552  2,551,104   3,811,736   32
    csv/Text.CSV/parseCSVFromFile              136,234,984  3,862,416  13,219,632  133
    sv/Data.Sv.Parse/attoparsecText            109,833,808  5,126,984  12,525,480  105
    sv/Data.Sv.Parse/attoparsecByteString       99,504,720  7,143,560  18,072,344   95
    sv/Data.Sv.Parse/trifecta                  270,308,144  9,468,328  24,254,240  259
    
    bench-0: Test suite space passed
    bench-0: benchmarks
    Running 1 benchmarks...
    Benchmark time: RUNNING...
    benchmarking file (time)/blitz/parseFile
    time                 1.611 ms   (1.593 ms .. 1.630 ms)
                         0.999 R²   (0.998 R² .. 0.999 R²)
    mean                 1.614 ms   (1.598 ms .. 1.634 ms)
    std dev              63.59 μs   (50.96 μs .. 80.28 μs)
    variance introduced by outliers: 27% (moderately inflated)
    
    benchmarking file (time)/cassava/decode/Vector ByteString
    time                 2.247 ms   (2.229 ms .. 2.267 ms)
                         0.999 R²   (0.998 R² .. 0.999 R²)
    mean                 2.274 ms   (2.254 ms .. 2.305 ms)
    std dev              79.82 μs   (58.99 μs .. 117.4 μs)
    variance introduced by outliers: 21% (moderately inflated)

--- src/Blitz.hs
+++ src/Blitz.hs
@@ -10,86 +10,78 @@ import           Control.DeepSeq
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Char8 as S8
-import           Data.ByteString.Internal
-import           Data.ByteString.Unsafe
-import           Foreign.Marshal.Alloc
-import           Foreign.Marshal.Utils
-import           Foreign.Ptr
--- import           Debug.Trace
 import           GHC.Generics
 
-data Rows
-  = Cons {-# UNPACK #-}!ByteString Rows
+data Values
+  = Cons {-# UNPACK #-}!ByteString Values
   | Nil
-  deriving (Generic)
-instance NFData Rows
+  deriving (Generic, Show)
+instance NFData Values
 
-parseFile :: FilePath -> IO Rows
+
+parseFile :: FilePath -> IO [[ByteString]]
 parseFile fp = do
   bytes0 <- S.readFile fp
-  let dispatch bytes acc =
-        case S8.uncons bytes of
-          Nothing -> acc
-          Just ('"', bytes') ->
-            let string extended dropped =
-                  case S8.elemIndex '"' (S.drop dropped bytes') of
-                    Nothing -> error "Unterminated quoted field."
-                    Just idx ->
-                      case S8.uncons (S.drop (dropped + idx + 1) bytes') of
-                        Just ('"', _) ->
-                          trace
-                            ("DOUBLE + EXTEND" :: String)
-                            (string True (dropped + idx + 2))
-                        Just (',', _) ->
-                          let prefinalStr = (S.take (dropped + idx + 1) bytes')
-                              finalStr =
-                                if extended
-                                  then S.intercalate
-                                         "\""
-                                         (filter
-                                            (not . S.null)
-                                            (S8.split '"' prefinalStr))
-                                  else prefinalStr
-                          in trace
-                               ("QUOTED + COMMA " ++ show finalStr)
-                               (dispatch
-                                  (S.drop (dropped + idx + 2) bytes')
-                                  (Cons finalStr acc))
-                        Just ('\n', _) ->
-                          let prefinalStr = (S.take (dropped + idx + 1) bytes')
-                              finalStr =
-                                if extended
-                                  then S.intercalate
-                                         "\""
-                                         (filter
-                                            (not . S.null)
-                                            (S8.split '"' prefinalStr))
-                                  else prefinalStr
-                          in trace
-                               ("QUOTED + CRLF " ++ show finalStr)
-                               (dispatch
-                                  (S.drop (dropped + idx + 2) bytes')
-                                  (Cons finalStr acc))
-                        Just {} -> error "Expected end of file."
-                        Nothing -> error "End of file?"
-            in string False 0
-          Just _ ->
-            case (S8.elemIndex ',' bytes, S8.elemIndex '\n' bytes) of
-              (Just comma, Just n)
-                | comma < n ->
-                  let finalStr = S.take comma bytes
-                  in trace
-                       ("COMMA " ++ show finalStr)
-                       (dispatch (S.drop (comma + 1) bytes) (Cons finalStr acc))
-                | otherwise ->
-                  let finalStr = S.take comma bytes
-                  in trace
-                       ("CRLF " ++ show (S.take n bytes))
-                       (dispatch (S.drop (n + 1) bytes) (Cons finalStr acc))
-              (Nothing, Just n) -> trace ("EOF " ++ show (S.take n bytes)) acc
-              x -> error ("Inexhaustive case: " ++ show x)
-  pure (dispatch bytes0 Nil)
+  pure (parseByteString bytes0)
 
-trace :: p1 -> p2 -> p2
-trace !_ x = x
-{-# INLINE trace #-}
+parseByteString :: ByteString -> [[ByteString]]
+parseByteString bytes0 = dispatch bytes0 [] []
+  where
+    dispatch bytes columns rows =
+      case S8.uncons bytes of
+        Nothing -> columns : rows
+        Just ('"', bytes') ->
+          let string extended dropped =
+                case S8.elemIndex '"' (S.drop dropped bytes') of
+                  Nothing -> error "Unterminated quoted field."
+                  Just idx ->
+                    case S8.uncons (S.drop (dropped + idx + 1) bytes') of
+                      Just ('"', _) -> string True (dropped + idx + 2)
+                      Just (',', _) ->
+                        let prefinalStr = (S.take (dropped + idx + 1) bytes')
+                            finalStr =
+                              if extended
+                                then S.intercalate
+                                       "\""
+                                       (filter
+                                          (not . S.null)
+                                          (S8.split '"' prefinalStr))
+                                else prefinalStr
+                        in dispatch
+                             (S.drop (dropped + idx + 2) bytes')
+                             ((:) finalStr columns)
+                             rows
+                      Just ('\n', _) ->
+                        let prefinalStr = (S.take (dropped + idx + 1) bytes')
+                            finalStr =
+                              if extended
+                                then S.intercalate
+                                       "\""
+                                       (filter
+                                          (not . S.null)
+                                          (S8.split '"' prefinalStr))
+                                else prefinalStr
+                        in dispatch
+                             (S.drop (dropped + idx + 2) bytes')
+                             []
+                             (((:) finalStr columns) : rows)
+                      Just {} -> error "Expected end of file."
+                      Nothing -> error "End of file?"
+          in string False 0
+        Just _ ->
+          case (S8.elemIndex ',' bytes, S8.elemIndex '\n' bytes) of
+            (Just comma, Just n)
+              | comma < n ->
+                let finalStr = S.take comma bytes
+                in dispatch
+                     (S.drop (comma + 1) bytes)
+                     ((:) finalStr columns)
+                     rows
+              | otherwise ->
+                let finalStr = S.take comma bytes
+                in dispatch
+                     (S.drop (n + 1) bytes)
+                     []
+                     ((:) finalStr columns : rows)
+            (Nothing, Just n) -> ((:) (S.take n bytes) columns) : rows
+            x -> error ("Inexhaustive case: " ++ show x)
